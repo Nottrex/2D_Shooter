b810c7f199a2e8e4ae649e066a1c7804df7d828b
Nottrex <Lau.Nottrex@gmail.com>
2017-07-20 20:48:50 +0200

    Window

---
+++ src/client/GameView.java
@@ -0,0 +1,28 @@
+package client;
+
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.awt.GLJPanel;
+
+public class GameView extends GLJPanel implements GLEventListener {
+
+	@Override
+	public void init(GLAutoDrawable glAutoDrawable) {
+
+	}
+
+	@Override
+	public void reshape(GLAutoDrawable glAutoDrawable, int i, int i1, int i2, int i3) {
+
+	}
+
+	@Override
+	public void dispose(GLAutoDrawable glAutoDrawable) {
+
+	}
+
+	@Override
+	public void display(GLAutoDrawable glAutoDrawable) {
+
+	}
+}
---
+++ src/client/Window.java
@@ -0,0 +1,16 @@
+package client;
+
+import javax.swing.*;
+
+public class Window extends JFrame {
+
+	public Window() {
+		super("2D Shooter!");
+
+		setSize(800, 600);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setResizable(true);
+		setVisible(true);
+	}
+
+}
